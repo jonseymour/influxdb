4704e488d23008febf02c62361c071d0081e3268
Jon Seymour <jon@wildducktheories.com>
2016-02-15 21:08:39 +1100

    tsm: simplify interface to CommitSnapshot() and RollbackSnapshot()
    Now that there is only one thread allowed to be actively writing
    a snapshot at once, we can simplify the the commit logic to simply
    removing all active snapshots. It also means we do not need
    to scan for the committed snapshot since we are going to purge all of them.
    
    Note that Rollback doesn't touch the snapshot slice - it leaves the
    snapshot in place until a subsequent compaction succeeds at which
    point it will be removed.
    
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- tsdb/engine/tsm1/engine.go
+++ tsdb/engine/tsm1/engine.go
@@ -423,7 +423,7 @@ func (e *Engine) WriteSnapshot() error {
 func (e *Engine) writeSnapshotAndCommit(closedFiles []string, snapshot *Cache, compactor *Compactor) (err error) {
 	defer func() {
 		if err != nil {
-			e.Cache.RollbackSnapshot(snapshot)
+			e.Cache.RollbackSnapshot()
 		}
 	}()
 
@@ -444,7 +444,7 @@ func (e *Engine) writeSnapshotAndCommit(closedFiles []string, snapshot *Cache, c
 	}
 
 	// clear the snapshot from the in-memory cache, then the old WAL files
-	e.Cache.CommitSnapshot(snapshot)
+	e.Cache.CommitSnapshot()
 
 	if err := e.WAL.Remove(closedFiles); err != nil {
 		e.logger.Printf("error removing closed wal segments: %v", err)
