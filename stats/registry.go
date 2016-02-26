b91651a9ac7407765f1c315a6cd36b2121e8fa4d
Jon Seymour <jon@wildducktheories.com>
2016-02-26 23:54:40 +1100

    stats: Introduce a new stats API.
    The API has several goals:
    
    * retain an underlying expvar implementation
    * expose an easy to use API for runtime code to use
    * allow statistics to be forgotten once the object described by the statistics has been closed
    * avoid the need for a big bang conversion to the new API
    
    The API uses a builder pattern which requires that described objects declare
    the members of their own statistics set during a build phase before they are first
    used at runtime. Once the build phase is complete the statistics object is explicitly "opened"
    and then an interface is exposed that allows the runtime to set or add values to the
    set of statistics pre-declared during the build phase.
    
    This two phase approach allows us to reduce the amount of locking needed during
    the runtime phase since, by construction, we prevent certain classes
    of mutation during the runtime phase. We use the go type system to help
    enforce correct construction of objects by preventing access to the
    runtime interface until the object has first been defined, built and finally
    opened.

---
+++ stats/registry.go
@@ -0,0 +1,133 @@
+package stats
+
+import (
+	"expvar"
+	"sync"
+)
+
+// A singleton reference to the stats registry
+var Root Registry = &registry{
+	listeners: []func(Openable){},
+}
+
+// Provide an interface
+type Registry interface {
+
+	// Called to obtain a builder of new Statistics object.
+	NewBuilder(k string, n string, tags map[string]string) Builder
+
+	// Notify the registry that statistics object has been opened for the first time.
+	NotifyOpen(s Statistics)
+
+	// Register a listener for NotifyOpen events.
+	OnOpen(listener func(s Openable))
+
+	// Called to iterate over the registered statistics sets
+	Do(f func(s Statistics))
+}
+
+// manages the registry of all statistics sets
+type registry struct {
+	mu        sync.RWMutex
+	listeners []func(Openable)
+}
+
+// ensure the top level map is always registered
+func init() {
+	m := &expvar.Map{} // this map can't be replaced because it is a top level map
+	m.Init()
+
+	r := &expvar.Map{} // this map is replaceable, since it is a value of a map
+	r.Init()
+
+	m.Set("root", r)
+	expvar.Publish("influx", m)
+}
+
+// iterate over the registry executing the function
+// specified. after it executes, we check to see if
+// the set is now closed. if it is, then we
+// we rebuild the top level map.
+func (r *registry) Do(f func(s Statistics)) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	closed := map[string]bool{}
+	r.getRoot().Do(func(kv expvar.KeyValue) {
+		set := kv.Value.(Statistics)
+		if set.Refs() > 0 {
+			f(set)
+		}
+		if set.Refs() == 0 {
+			closed[kv.Key] = true
+		}
+	})
+
+	// rebuild the registry map
+	if len(closed) > 0 {
+
+		// if this ever becomes too expensive, we might arrange things so that the
+		// cleaning happens once the number of closed entries exceeds a certain
+		// number or percentage of the map size.
+
+		cleaned := &expvar.Map{}
+		cleaned.Init()
+		r.getRoot().Do(func(kv expvar.KeyValue) {
+			if !closed[kv.Key] {
+				cleaned.Set(kv.Key, kv.Value)
+			}
+		})
+		r.getInflux().Set("root", cleaned)
+	}
+}
+
+// get the "influx" map from expvar - this map is not replacable
+func (r *registry) getInflux() *expvar.Map {
+	return expvar.Get("influx").(*expvar.Map)
+}
+
+// get the "root" map from the "influx" map - this map is replaceable
+func (r *registry) getRoot() *expvar.Map {
+	return r.getInflux().Get("root").(*expvar.Map)
+}
+
+// create a new builder
+func (r *registry) NewBuilder(k string, n string, tags map[string]string) Builder {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	impl := &expvar.Map{}
+	impl.Init()
+
+	builder := &statistics{
+		key:        k,
+		registry:   r,
+		name:       n,
+		tags:       tags,
+		impl:       impl,
+		refs:       0,
+		intVars:    map[string]*expvar.Int{},
+		stringVars: map[string]*expvar.String{},
+		floatVars:  map[string]*expvar.Float{},
+	}
+
+	return builder
+}
+
+func (r *registry) NotifyOpen(s Statistics) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	for _, f := range r.listeners {
+		f(s)
+	}
+	r.getRoot().Set(s.Key(), s)
+	return
+}
+
+func (r *registry) OnOpen(l func(o Openable)) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.listeners = append(r.listeners, l)
+}
---
+++ stats/registry_test.go
@@ -0,0 +1,69 @@
+package stats_test
+
+import (
+	"testing"
+
+	"github.com/influxdata/influxdb/stats"
+)
+
+func init() {
+	stats.Root.OnOpen(func(o stats.Openable) {
+		_ = o.Open()
+	})
+}
+
+func TestEmptyStatistics(t *testing.T) {
+	found := make([]stats.Statistics, 0)
+	stats.Root.Do(func(s stats.Statistics) {
+		found = append(found, s)
+	})
+
+	if length := len(found); length != 0 {
+		t.Fatalf("non empty initial state. got %d, expected: %d", length, 0)
+	}
+}
+
+// Test that we can create one statistic and that it disappears after it is deleted twice.
+func TestOneStatistic(t *testing.T) {
+
+	foo := stats.Root.
+		NewBuilder("foo", "bar", map[string]string{"tag": "T"}).
+		MustBuild().
+		Open()
+
+	found := make([]stats.Statistics, 0)
+	stats.Root.Do(func(s stats.Statistics) {
+		found = append(found, s)
+	})
+
+	if len(found) != 1 {
+		t.Fatalf("enumeration error after do. length of slice: got %d, expected %d", len(found), 1)
+	}
+	m := found[0]
+	if fooActual := m; fooActual != foo {
+		t.Fatalf("failed to obtain expected map. got: %v, expected: %v", fooActual, foo)
+	}
+
+	foo.Close()
+
+	found = make([]stats.Statistics, 0)
+	stats.Root.Do(func(s stats.Statistics) {
+		found = append(found, s)
+		if s.Refs() == 1 {
+			s.Close()
+		}
+	})
+
+	if length := len(found); length != 1 {
+		t.Fatalf("failed to find expected number of objects. got: %d, expected: 1", length)
+	}
+
+	found = make([]stats.Statistics, 0)
+	stats.Root.Do(func(s stats.Statistics) {
+		found = append(found, s)
+	})
+
+	if length := len(found); length != 0 {
+		t.Fatalf("failed to find expected number of objects. got: %d, expected: 0", length)
+	}
+}
