ee96956f9a2ae691ed36a59040557dea648c8466
Jon Seymour <jon@wildducktheories.com>
2016-02-26 23:54:40 +1100

    stats: Introduce a new stats API.
    The API has several goals:
    
    * retain an underlying expvar implementation
    * expose an easy to use API for runtime code to use
    * allow statistics to be forgotten once the object described by the statistics has been closed
    * avoid the need for a big bang conversion to the new API
    
    The API uses a builder pattern which requires that described objects declare
    the members of their own statistics set during a build phase before they are first
    used at runtime. Once the build phase is complete the statistics object is explicitly "opened"
    and then an interface is exposed that allows the runtime to set or add values to the
    set of statistics pre-declared during the build phase.
    
    This two phase approach allows us to reduce the amount of locking needed during
    the runtime phase since, by construction, we prevent certain classes
    of mutation during the runtime phase. We use the go type system to help
    enforce correct construction of objects by preventing access to the
    runtime interface until the object has first been defined, built and finally
    opened.
    
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

---
+++ stats/lifecycle.go
@@ -0,0 +1,105 @@
+package stats
+
+// Return true if there is less than 2 references to the receiver
+func (s *statistics) Open() Statistics {
+	var notify bool
+
+	s.mu.Lock()
+	s.refs++
+	notify = (s.refs == 1)
+	s.mu.Unlock()
+
+	// Perform this notification outside of a lock.
+	// Inside of a lock, there is no room to move.
+	//
+	// With apologies to Groucho Marx.
+	if notify {
+		s.registry.NotifyOpen(s)
+	}
+	return s
+}
+
+// Return true if there is less than 2 references to the receiver
+func (s *statistics) Refs() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return s.refs
+}
+
+// Release one reference to the receiver.
+func (s *statistics) Close() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.refs--
+}
+
+// Used by newly opened Statistics objects to notify OnOpen
+// listeners that a new Statistics object has been registered.
+func (r *registry) NotifyOpen(s Statistics) {
+
+	// clone the list of listeners
+	r.mu.RLock()
+	clone := make([]*listener, len(r.listeners))
+	copy(clone, r.listeners)
+	r.mu.RUnlock()
+
+	// call the each of the cloned listeners without holding any lock
+	for _, l := range clone {
+		l.callback(s)
+	}
+
+	// update the statistics map while holding the write lock
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.getStatistics().Set(s.Key(), s)
+
+	return
+}
+
+// Register a new OnOpen listener. The listener will receive notifications for
+// all open Statistics currently in the Registry and for any objects that are
+// subsequently added.
+func (r *registry) OnOpen(lf func(o Openable)) func() {
+
+	existing := []Statistics{}
+
+	// add a new listener while holding the write lock
+	r.mu.Lock()
+	l := &listener{
+		callback: lf,
+	}
+	l.closer = func() {
+		r.mu.Lock()
+		defer r.mu.Unlock()
+
+		for i, e := range r.listeners {
+			if e == l {
+				r.listeners = append(r.listeners[:i], r.listeners[i+1:]...)
+				return
+			}
+		}
+	}
+
+	r.do(func(s Statistics) {
+		existing = append(existing, s)
+	})
+
+	r.listeners = append(r.listeners, l)
+	r.mu.Unlock()
+
+	// Call the listener on objects that were already in the map before we added a listener.
+	for _, s := range existing {
+		if s.Refs() > 0 {
+			lf(s)
+		}
+	}
+
+	// By the time we get here, the listener has received one notification for
+	// each Statistics object that was in the map prior to the listener being registered
+	// and one notification for each added since. The notifications won't necessarily be received
+	// in order of their original delivery to other listeners.
+
+	return l.closer
+}
