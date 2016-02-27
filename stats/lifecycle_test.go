b22fb63af4eab6deb317f648d91aaea5f4a29a58
Jon Seymour <jon@wildducktheories.com>
2016-02-27 12:43:59 +1100

    squash-me: stats: test that statistics after observer are added are seen.
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- stats/lifecycle_test.go
+++ stats/lifecycle_test.go
@@ -31,3 +31,28 @@ func TestNotifyOpenOrderStatisticFirst(t *testing.T) {
 		t.Fatalf("did not observe existing statistic. got: %+v, expected: %+v", observed, expected)
 	}
 }
+
+func TestNotifyOpenOrderObserverFirst(t *testing.T) {
+	observed := []stats.Statistics{}
+	defer func() {
+		for _, e := range observed {
+			e.Close()
+		}
+	}()
+
+	closer := stats.Root.OnOpen(func(s stats.Openable) {
+		observed = append(observed, s.Open())
+	})
+	defer closer()
+
+	stat := stats.Root.
+		NewBuilder("key", "name", map[string]string{"tag": "T"}).
+		MustBuild().
+		Open()
+	defer stat.Close()
+
+	expected := []stats.Statistics{stat}
+	if !reflect.DeepEqual(expected, observed) {
+		t.Fatalf("did not observe new statistic. got: %+v, expected: %+v", observed, expected)
+	}
+}
