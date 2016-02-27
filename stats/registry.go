c2adb7b9715511edef8cacfc84df4a3a47474e68
Jon Seymour <jon@wildducktheories.com>
2016-02-27 11:24:57 +1100

    squash-me: stats: registry: various cleanups
    * rename listenerTicker to listener
    * rename statistics expvar map from "root" to "statistics"
    * ensure notifications received for existing Statistics at time of OnOpen call
    * ensure notifications processed without holding Statistics lock
    * factored out do() from Do() to allow re-use
    * factored out cleaning logic into its own method
    * be precise about which locks are used and for what purpose

--- stats/registry.go
+++ stats/registry.go
@@ -7,33 +7,42 @@ import (
 
 // A singleton reference to the stats registry
 var Root Registry = &registry{
-	listeners: make([]*listenerTicket, 0),
+	listeners: make([]*listener, 0),
 }
 
 // Provide an interface
 type Registry interface {
 
-	// Called to obtain a builder of new Statistics object.
+	// Create a new Builder of statistics objects.
 	NewBuilder(k string, n string, tags map[string]string) Builder
 
-	// Notify the registry that statistics object has been opened for the first time.
+	// Notify listeners that a new Statistics object has been opened for the first time.
 	NotifyOpen(s Statistics)
 
 	// Register a listener for NotifyOpen events. The returned function will
 	// deregister the listener when called.
+	//
+	// Listeners are always invoked on goroutines that currently do not hold
+	// any locks over the Registry.
 	OnOpen(listener func(s Openable)) func()
 
 	// Called to iterate over the registered statistics sets
 	Do(f func(s Statistics))
 }
 
-// manages the registry of all statistics sets
-type registry struct {
-	mu        sync.RWMutex
-	listeners []*listenerTicket
+// A type used to allow callbacks to be deregistered
+type listener struct {
+	callback func(Openable)
+	closer   func()
 }
 
-// ensure the top level map is always registered
+// A type used to represent a registry of all Statistics objects
+type registry struct {
+	mu        sync.RWMutex
+	listeners []*listener
+}
+
+// Ensure the top level map is always registered.
 func init() {
 	m := &expvar.Map{} // this map can't be replaced because it is a top level map
 	m.Init()
@@ -41,45 +50,66 @@ func init() {
 	r := &expvar.Map{} // this map is replaceable, since it is a value of a map
 	r.Init()
 
-	m.Set("root", r)
+	m.Set("statistics", r)
 	expvar.Publish("influx", m)
 }
 
-// iterate over the registry executing the function
-// specified. after it executes, we check to see if
-// the set is now closed. if it is, then we
-// we rebuild the top level map.
-func (r *registry) Do(f func(s Statistics)) {
+// Cleans the registry to remove statistics that have been closed.
+func (r *registry) clean() {
+
+	// rebuild the registry map
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	closed := map[string]struct{}{}
-	r.getRoot().Do(func(kv expvar.KeyValue) {
-		set := kv.Value.(Statistics)
-		if set.Refs() > 0 {
-			f(set)
-		}
-		if set.Refs() == 0 {
-			closed[kv.Key] = struct{}{}
+	cleaned := &expvar.Map{}
+	cleaned.Init()
+	r.do(func(stats Statistics) {
+		if stats.Refs() > 0 {
+			cleaned.Set(stats.Key(), stats)
 		}
 	})
+	r.getInflux().Set("statistics", cleaned)
+}
 
-	// rebuild the registry map
-	if len(closed) > 0 {
+//
+// Iterates over the registry, holding a read lock.
+//
+// The iteration skips over closed statistics.
+//
+// If any closed statistics are detected during
+// the operation, then the "statistics" map is
+// cleansed by creating a new map and copying
+// only those statistics that are still open.
+//
+func (r *registry) Do(f func(s Statistics)) {
 
-		// if this ever becomes too expensive, we might arrange things so that the
-		// cleaning happens once the number of closed entries exceeds a certain
-		// number or percentage of the map size.
+	count := 0
+	r.mu.RLock()
+	r.do(func(stats Statistics) {
+		if stats.Refs() > 0 {
+			f(stats)
+		}
+		if stats.Refs() == 0 {
+			count++
+		}
+	})
+	r.mu.RUnlock()
 
-		cleaned := &expvar.Map{}
-		cleaned.Init()
-		r.getRoot().Do(func(kv expvar.KeyValue) {
-			if _, closed := closed[kv.Key]; !closed {
-				cleaned.Set(kv.Key, kv.Value)
-			}
-		})
-		r.getInflux().Set("root", cleaned)
+	if count > 0 {
+		r.clean()
 	}
+}
+
+// Iterate over all statistics irrespective of
+// whether they are closed or not and without
+// any cleaning behaviour.
+//
+// The caller is responsible for acquiring an appropriate
+// lock.
+func (r *registry) do(f func(s Statistics)) {
+	r.getStatistics().Do(func(kv expvar.KeyValue) {
+		f(kv.Value.(Statistics))
+	})
 }
 
 // get the "influx" map from expvar - this map is not replacable
@@ -87,22 +117,20 @@ func (r *registry) getInflux() *expvar.Map {
 	return expvar.Get("influx").(*expvar.Map)
 }
 
-// get the "root" map from the "influx" map - this map is replaceable
-func (r *registry) getRoot() *expvar.Map {
-	return r.getInflux().Get("root").(*expvar.Map)
+// get the "statistics" map from the "influx" map - this map is replaceable
+func (r *registry) getStatistics() *expvar.Map {
+	return r.getInflux().Get("statistics").(*expvar.Map)
 }
 
-// create a new builder
+// Create a new builder that retains a reference to the registry.
 func (r *registry) NewBuilder(k string, n string, tags map[string]string) Builder {
-	r.mu.Lock()
-	defer r.mu.Unlock()
 
 	impl := &expvar.Map{}
 	impl.Init()
 
 	builder := &statistics{
-		key:        k,
 		registry:   r,
+		key:        k,
 		name:       n,
 		tags:       tags,
 		impl:       impl,
@@ -115,41 +143,71 @@ func (r *registry) NewBuilder(k string, n string, tags map[string]string) Builde
 	return builder
 }
 
+// Used by newly opened Statistics objects to notify OnOpen
+// listeners that a new Statistics object has been registered.
 func (r *registry) NotifyOpen(s Statistics) {
+
+	// clone the list of listeners
+	r.mu.RLock()
+	clone := make([]*listener, len(r.listeners))
+	copy(clone, r.listeners)
+	r.mu.RUnlock()
+
+	// call the each of the cloned listeners without holding any lock
+	for _, l := range clone {
+		l.callback(s)
+	}
+
+	// update the statistics map while holding the write lock
 	r.mu.Lock()
 	defer r.mu.Unlock()
+	r.getStatistics().Set(s.Key(), s)
 
-	for _, t := range r.listeners {
-		t.callback(s)
-	}
-	r.getRoot().Set(s.Key(), s)
 	return
 }
 
-func (r *registry) OnOpen(l func(o Openable)) func() {
-	r.mu.Lock()
-	defer r.mu.Unlock()
+// Register a new OnOpen listener. The listener will receive notifications for
+// all open Statistics currently in the Registry and for any objects that are
+// subsequently added.
+func (r *registry) OnOpen(lf func(o Openable)) func() {
 
-	ticket := &listenerTicket{
-		callback: l,
+	existing := []Statistics{}
+
+	// add a new listener while holding the write lock
+	r.mu.Lock()
+	l := &listener{
+		callback: lf,
 	}
-	ticket.closer = func() {
+	l.closer = func() {
 		r.mu.Lock()
 		defer r.mu.Unlock()
 
 		for i, e := range r.listeners {
-			if e == ticket {
+			if e == l {
 				r.listeners = append(r.listeners[:i], r.listeners[i+1:]...)
 				return
 			}
 		}
-
 	}
-	r.listeners = append(r.listeners, ticket)
-	return ticket.closer
-}
 
-type listenerTicket struct {
-	callback func(Openable)
-	closer   func()
+	r.do(func(s Statistics) {
+		existing = append(existing, s)
+	})
+
+	r.listeners = append(r.listeners, l)
+	r.mu.Unlock()
+
+	// Call the listener on objects that were already in the map before we added a listener.
+	for _, s := range existing {
+		if s.Refs() > 0 {
+			lf(s)
+		}
+	}
+
+	// By the time we get here, the listener has received one notification for
+	// each Statistics object that was in the map prior to the listener being registered
+	// and one notification for each added since. The notifications won't necessarily be received
+	// in order of their original delivery to other listeners.
+
+	return l.closer
 }
