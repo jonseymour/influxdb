bdb2b598fca5a1fa17ee456053328072fda77da2
Jon Seymour <jon@wildducktheories.com>
2016-02-27 12:31:31 +1100

    squash-me: stats: test that observe sees existing statistics.

---
+++ stats/lifecycle_test.go
@@ -0,0 +1,33 @@
+package stats_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/influxdata/influxdb/stats"
+)
+
+func TestNotifyOpenOrderStatisticFirst(t *testing.T) {
+	stat := stats.Root.
+		NewBuilder("key", "name", map[string]string{"tag": "T"}).
+		MustBuild().
+		Open()
+	defer stat.Close()
+
+	observed := []stats.Statistics{}
+	defer func() {
+		for _, e := range observed {
+			e.Close()
+		}
+	}()
+
+	closer := stats.Root.OnOpen(func(s stats.Openable) {
+		observed = append(observed, s.Open())
+	})
+	defer closer()
+
+	expected := []stats.Statistics{stat}
+	if !reflect.DeepEqual(expected, observed) {
+		t.Fatalf("did not observe existing statistic. got: %+v, expected: %+v", observed, expected)
+	}
+}
