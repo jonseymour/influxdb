681d8e28b49598630dc86efb65f18e7dfc55882e
Jon Seymour <jon@wildducktheories.com>
2016-02-28 10:02:20 +1100

    stats: re-implement influxvar and monitor/service in terms of new 'stats' API
    We can considerably simplify the implementation of influxvar.go and monitor/service.go
    by making use of the new API. Rather than using a tree of weakly typed
    expvar maps, we use the strong types provided by the new API.
    
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

---
+++ influxstats.go
@@ -0,0 +1,19 @@
+package influxdb
+
+import (
+	"expvar"
+	"github.com/influxdata/influxdb/stats"
+)
+
+// Deprecated - to be removed.
+//
+// Use stats.Root.NewBuilder(...).MustBuild().Open().Map() instead.
+//
+// NewStatistics returns an expvar-based map with the given key.
+func NewStatistics(key, name string, tags map[string]string) *expvar.Map {
+	return stats.Root.
+		NewBuilder(key, name, tags).
+		MustBuild().
+		Open().
+		Map()
+}
