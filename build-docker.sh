98c19b285b562ed27c3d1c283c6a130215de475c
Jon Seymour <jon@wildducktheories.com>
2016-04-04 15:31:09 +1000

    Don't compile with cgo disabled.
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- build-docker.sh
+++ build-docker.sh
@@ -5,6 +5,6 @@ set -e -x
 GO_VER=${GO_VER:-1.4.3}
 
 docker run -v "${GOPATH}":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:$GO_VER sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o influxd ./cmd/influxd'
-docker run -v "${GOPATH}":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:$GO_VER sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o influx ./cmd/influx'
+docker run -v "${GOPATH}":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:$GO_VER sh -c 'go build --ldflags="-s" -o influx ./cmd/influx'
 
 docker build -t influxdb .
